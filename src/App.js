3c553f606696b5596bf4c02e615d21c83dccb826
othman19t <othman19t@gmail.com>
2020-06-04 21:39:18 -0700

    despatching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import store from "./store";
 
 function App() {
+  store.dispatch({ type: "bugAdded", payload: { description: "first Bug" } });
+  console.log(store.getState());
+  store.dispatch({ type: "bugRemoved", payload: { id: 2 } });
+  console.log(store.getState());
+
   return (
     <div className="App">
-     <h1>hello world</h1>
+      <h1>hello world</h1>
     </div>
   );
 }
